a4b21591e5defb12224c2dfcb62dbcfeee687f00
AGkal01 <55670862+AGkal01@users.noreply.github.com>
2019-09-23 13:14:42 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -8,9 +8,6 @@ import random
 
 class ZergAgent(base_agent.BaseAgent):
 
-  global n_attack  
-  global n_bild 
-  global n_attack2 
 
   n_attack = 0 
   n_bild = 0
@@ -26,8 +23,7 @@ class ZergAgent(base_agent.BaseAgent):
     self.n_attacks = False
     self.S_base = False 
 
-    #decalra las variables globales  
- 
+
     
   def unit_type_is_selected(self, obs, unit_type):
   
@@ -58,9 +54,9 @@ class ZergAgent(base_agent.BaseAgent):
 
 
   def move_camera_S_B(self):
-    #if there is no barraks (spawning pool) build one
+    #you can  see other part of the map in the coordinates of the minimap 
     camaramove = actions.FUNCTIONS.move_camera(self.second_base) 
-        # if drone is selected build spawning pool 
+       
     return camaramove 
 
   def my_attack1(self, obs):
@@ -83,7 +79,7 @@ class ZergAgent(base_agent.BaseAgent):
 
 
   def my_attack(self, obs):
-    #if enough zerglings,send attack
+    #if enough hydras and zerlings ,send attack
 
     zerglings = self.get_units_by_type(obs, units.Zerg.Zergling)
     hydras = self.get_units_by_type(obs, units.Zerg.Hydralisk)
@@ -95,7 +91,7 @@ class ZergAgent(base_agent.BaseAgent):
                 return actions.FUNCTIONS.Attack_minimap("now", 
                                                         self.attack_coordinates)
         
-        #select zerglings
+        #select all the army 
         if self.can_do(obs, actions.FUNCTIONS.select_army.id):
             return actions.FUNCTIONS.select_army("select")
  
@@ -123,13 +119,13 @@ class ZergAgent(base_agent.BaseAgent):
         if len(drones) > 0 :
             drone = random.choice(drones)
                     
-            return actions.FUNCTIONS.select_point("select_all_type",(drone.x, 
-                                                                  drone.y))
-# el den es el edificio que crea los Hydralisk 
+            return actions.FUNCTIONS.select_point("select_all_type",(drone.x, drone.y))
+
+# The Hydralisk Den is a Zerg building that allows the Zerg player to create Hydralisks. 
   def my_den(self, obs):
     den = self.get_units_by_type(obs, units.Zerg.HydraliskDen)
     spawning_pools = self.get_units_by_type(obs, units.Zerg.SpawningPool)
-
+# if you don't have a Hydralisk Den bild one
     if len (den) == 0 :
         if self.unit_type_is_selected(obs, units.Zerg.Drone):        
             if self.can_do(obs,actions.FUNCTIONS.Build_HydraliskDen_screen.id):
@@ -146,9 +142,13 @@ class ZergAgent(base_agent.BaseAgent):
                     
             return actions.FUNCTIONS.select_point("select_all_type",(drone.x, drone.y))
 
+#The Infestation Pit is a second-tier Zerg building that unlocks the Hive, Infestor,
+#Swarm Host, as well as the Infestor and Swarm Host upgrades.
+
   def my_InfestationPit(self, obs):
     InfestationPit = self.get_units_by_type(obs, units.Zerg.InfestationPit)
 
+# if you don't have a InfestationPit bild one
     if len (InfestationPit) == 0 :
         if self.unit_type_is_selected(obs, units.Zerg.Drone):        
             if self.can_do(obs,actions.FUNCTIONS.Build_InfestationPit_screen.id):
@@ -167,13 +167,12 @@ class ZergAgent(base_agent.BaseAgent):
                                                                   drone.y))
 
 
-
+#The Lair is a Zerg building that is the direct upgrade from a Hatchery.
   def my_lair(self, obs):
-    #lair is the morph fase of the hatchery 
+
     lair = self.get_units_by_type(obs, units.Zerg.Lair)
     hatchery = self.get_units_by_type(obs, units.Zerg.Hatchery)
     if len (lair) == 0 or (hatchery == 1 ):
-        # if drone is selected build spawning pool
         if self.unit_type_is_selected(obs, units.Zerg.Hatchery):        
             if self.can_do(obs,actions.FUNCTIONS.Morph_Lair_quick.id):
                 return actions.FUNCTIONS.Morph_Lair_quick("now")
@@ -186,13 +185,11 @@ class ZergAgent(base_agent.BaseAgent):
             return actions.FUNCTIONS.select_point("select_all_type",(hatchery.x, hatchery.y))
 
 
-
+#The Hive is the final upgrade to the Hatchery 
   def my_Hive(self, obs):
-    #Hive is the morph fase of the Lair
     Hive = self.get_units_by_type(obs, units.Zerg.Hive)
     Lair = self.get_units_by_type(obs, units.Zerg.Lair)
     if len (Hive) == 0:
-        # if drone is selected build spawning pool
         if self.unit_type_is_selected(obs, units.Zerg.Lair):        
             if self.can_do(obs,actions.FUNCTIONS.Morph_Hive_quick.id):
                 return actions.FUNCTIONS.Morph_Hive_quick("now")
@@ -204,22 +201,17 @@ class ZergAgent(base_agent.BaseAgent):
                     
             return actions.FUNCTIONS.select_point("select_all_type",(Lair.x, Lair.y))
 
+#he Extractor is the Zerg building from which Drones collect Vespene Gas. 
   def my_extractor(self, obs):
-    #if there is no barraks (spawning pool) build one
     extractor = self.get_units_by_type(obs, units.Zerg.Extractor)
     geysers = self.get_units_by_type(obs, units.Neutral.VespeneGeyser)
 
     if len (extractor) != len(geysers) :
-        # if drone is selected build spawning pool
         if self.unit_type_is_selected(obs, units.Zerg.Drone):        
             if self.can_do(obs,actions.FUNCTIONS.Build_Extractor_screen.id):
                 geysers = self.get_units_by_type(obs, units.Neutral.VespeneGeyser)
                 if len(geysers) > 0 :
                     geyser = random.choice(geysers)
-                    #VespeneGeyser
-
-
-
                     return actions.FUNCTIONS.Build_Extractor_screen("now", (geyser.x,geyser.y))
             
         # select some random drone for the next choice
@@ -245,30 +237,9 @@ class ZergAgent(base_agent.BaseAgent):
                     drone = random.choice(drones)                            
                     return actions.FUNCTIONS.select_point("select",(drone.x,drone.y))
 
-  def my_Second_base(self, obs):
-    #if there is no barraks (spawning pool) build one
-    camaramove = actions.FUNCTIONS.move_camera(self.second_base) 
-    Hatchery = self.get_units_by_type(obs, units.Zerg.Hatchery)
-    if self.S_base == False:
-        # if drone is selected build spawning pool
-        if self.unit_type_is_selected(obs, units.Zerg.Drone):       
-            return camaramove 
-            if self.can_do(obs,actions.FUNCTIONS.Build_Hatchery_screen.id):
-                self.S_base = True
-
-                return actions.FUNCTIONS.Build_Hatchery_screen("now", 
-                                                        self.second_base)
-            
-        # select some random drone for the next choice
-        drones = self.get_units_by_type(obs, units.Zerg.Drone)                
-        if len(drones) > 0 :
-            drone = random.choice(drones)
- 
-            return actions.FUNCTIONS.select_point("select_all_type",(drone.x, drone.y))
-
-
+#The Spine Crawler is a Zerg static defense structure 
   def my_SpineCrawler(self, obs):
-    #if there is no Roachwarren (spawning pool) build one
+    #if there is no SpineCrawler build one
     SpineCrawler = self.get_units_by_type(obs, units.Zerg.SpineCrawler)
     if len (SpineCrawler) < 2:
         # if drone is selected build spawning pool
@@ -277,7 +248,6 @@ class ZergAgent(base_agent.BaseAgent):
                 x = random.randint(50,60)
                 y = random.randint(5,60)
      
-
                 return actions.FUNCTIONS.Build_SpineCrawler_screen ("now", (y,x))
             
         # select some random drone for the next choice
@@ -287,14 +257,12 @@ class ZergAgent(base_agent.BaseAgent):
                     
             return actions.FUNCTIONS.select_point("select_all_type",(drone.x, drone.y))
 
-
-
-
+#The Ultralisk Cavern is one of two Zerg tech buildings that are available after the completion of Hive.
   def my_UltraliskCavern(self, obs):
-    #if there is no Roachwarren (spawning pool) build one
+    #if there is no UltraliskCavern build one
     UltraliskCavern = self.get_units_by_type(obs, units.Zerg.UltraliskCavern)
     if len (UltraliskCavern) == 0 :
-        # if drone is selected build spawning pool
+        # if drone is selected build UltraliskCavern
         if self.unit_type_is_selected(obs, units.Zerg.Drone):        
             if self.can_do(obs,actions.FUNCTIONS.Build_UltraliskCavern_screen.id):
                 x = random.randint(0,63)
@@ -311,10 +279,9 @@ class ZergAgent(base_agent.BaseAgent):
 
 
   def my_Hatchery_2(self, obs):
-    #if there is no barraks (spawning pool) build one
     Hatchery = self.get_units_by_type(obs, units.Zerg.Hatchery)
     if len (Hatchery) == 0 :
-        # if drone is selected build spawning pool
+        # if drone is selected build Hatchery
         if self.unit_type_is_selected(obs, units.Zerg.Drone):        
             if self.can_do(obs,actions.FUNCTIONS.Build_Hatchery_screen.id):
                 x = random.randint(0,80)
@@ -331,39 +298,6 @@ class ZergAgent(base_agent.BaseAgent):
             return actions.FUNCTIONS.select_point("select_all_type",(drone.x, drone.y))
 
 
-
-            #this is an update of the Hydras units 
-  def up_groveedSpines(self, obs):
-    #lair is the morph fase of the hatchery 
-    den = self.get_units_by_type(obs, units.Zerg.HydraliskDen)
-    if len (den) > 0  :
-        print("entro")
-        # if drone is selected build spawning pool
-        if self.unit_type_is_selected(obs, units.Zerg.HydraliskDen):  
-
-            print("yes_is_hydralisk")     
-            if self.can_do(obs,actions.FUNCTIONS.Research_GroovedSpines_quick.id):
-                (print(self.can_do(obs,actions.FUNCTIONS.Research_GroovedSpines_quick.id)))
-                
-                print("can d0")
-                return actions.FUNCTIONS.Research_GroovedSpines_quick("now")
-
-
-            return actions.FUNCTIONS.select_point("select_all_type",(HydraliskDen.x, HydraliskDen.y))
-
-
-  def up_MuscularAugments(self, obs):
-    #lair is the morph fase of the hatchery 
-    lair = self.get_units_by_type(obs, units.Zerg.Lair)
-    if len (lair) > 0 :
-        # if drone is selected build spawning pool
-        if self.unit_type_is_selected(obs, units.Zerg.HydraliskDen):        
-            if self.can_do(obs,actions.FUNCTIONS.Research_MuscularAugments_quick.id):
-                return actions.FUNCTIONS.Research_MuscularAugments_quick("now")
-
-                    
-            return actions.FUNCTIONS.select_point("select_all_type",(HydraliskDen.x, HydraliskDen.y))
-
   def my_more_units(self, obs, type):
     #make units
     if self.unit_type_is_selected(obs, units.Zerg.Larva):
@@ -375,23 +309,22 @@ class ZergAgent(base_agent.BaseAgent):
                 return actions.FUNCTIONS.Train_Overlord_quick("now")
 
         if type == "zergling":
-            # if it is possible build troops       
+            # if it is possible build troops zergling      
             if self.can_do(obs, actions.FUNCTIONS.Train_Zergling_quick.id):
                     return actions.FUNCTIONS.Train_Zergling_quick("now")
         
         if type == "drone":
+            # if it is possible build troops drone  
             if self.can_do(obs, actions.FUNCTIONS.Train_Drone_quick.id):
                     return actions.FUNCTIONS.Train_Drone_quick("now")
                     
         if type == "hydralisk":
-            
-
+            # if it is possible build troops hydralisk 
             if self.can_do(obs, actions.FUNCTIONS.Train_Hydralisk_quick.id):
                     return actions.FUNCTIONS.Train_Hydralisk_quick("now")
 
-
-        if type == "Ultralisk":
-            
+        if type == "Ultralisk":   
+             # if it is possible build troops Ultralisk
             if self.can_do(obs, actions.FUNCTIONS.Train_Ultralisk_quick.id):
                     return actions.FUNCTIONS.Train_Ultralisk_quick("now")
 
@@ -400,8 +333,7 @@ class ZergAgent(base_agent.BaseAgent):
     if len(larvae) > 0 :
         larva = random.choice(larvae)
 
-        return actions.FUNCTIONS.select_point("select_all_type", (larva.x, 
-                                                                   larva.y))
+        return actions.FUNCTIONS.select_point("select_all_type", (larva.x, larva.y))
 
 
   def step(self, obs):
@@ -447,7 +379,7 @@ class ZergAgent(base_agent.BaseAgent):
     if extractor:
         return extractor
     
-        #harvest gas
+    #harvest gas
     gas = self.my_harvest_gas(obs)
     if gas:
         return gas
@@ -466,31 +398,33 @@ class ZergAgent(base_agent.BaseAgent):
     #camaramove = self.move_camera_S_B()
     #return camaramove 
 
+    #train a Queen 
     queens = self.get_units_by_type(obs, units.Zerg.Queen)
     if len(queens) <1 :
         if self.unit_type_is_selected(obs, units.Zerg.Lair):        
             if self.can_do(obs,actions.FUNCTIONS.Train_Queen_quick.id):
                     return actions.FUNCTIONS.Train_Queen_quick("now")
 
-        #build 
+    #build a Hydralisden
     den = self.my_den(obs)
     if den:
         return den
 
+    #build a InfestationPit
     InfestationPit = self.my_InfestationPit(obs)
     if InfestationPit:
         return InfestationPit
 
+    #build a Hive
     Hive = self.my_Hive(obs)
     if Hive:
         return Hive
 
-
+    #build a UltraliskCavern
     Ultra = self.my_UltraliskCavern(obs)
     if Ultra:
         return Ultra
 
-    # siempre tener una reina, dado que es la que acelera la creacion de tropas 
     queens = self.get_units_by_type(obs, units.Zerg.Queen)
     print ("numero de reina",len(queens))
     if len(queens) <1:
@@ -499,25 +433,20 @@ class ZergAgent(base_agent.BaseAgent):
                 return actions.FUNCTIONS.Train_Queen_quick("now")
         
     else :
-        zerglings =  self.get_units_by_type(obs, units.Zerg.Zergling)
-        hydras =  self.get_units_by_type(obs, units.Zerg.Hydralisk)
-        Ultralisk =  self.get_units_by_type(obs, units.Zerg.Ultralisk)
         
-
-                #make more hydras
-        #python always includes by default "self" as a parameter in the call
+        Ultralisk =  self.get_units_by_type(obs, units.Zerg.Ultralisk)
         zerglings =  self.get_units_by_type(obs, units.Zerg.Zergling)
         hydras = self.get_units_by_type(obs, units.Zerg.Hydralisk)
+        Hatchery = self.get_units_by_type(obs, units.Zerg.Hatchery)
+        Hive = self.get_units_by_type(obs, units.Zerg.Hive)
+
         print ("numero de hydras ", len(hydras))
         print ("numero de zerglings ", len(zerglings))
 
-        Hatchery = self.get_units_by_type(obs, units.Zerg.Hatchery)
-        Hive = self.get_units_by_type(obs, units.Zerg.Hive)
         if len(Hive) == 1 and len(Hatchery) == 0 :
             Hatchery = self.my_Hatchery_2(obs)
             if Hatchery:
                 return Hatchery
-
 
         if len (hydras) < 9  :
             zergling_attack = self.my_attack1(obs)  
@@ -549,10 +478,6 @@ class ZergAgent(base_agent.BaseAgent):
             make_units = self.my_more_units(obs,"hydralisk")
             if make_units:
                 return make_units
-
-        
-
-
 
     return actions.FUNCTIONS.no_op()  #do not do anything if there were no matches
 
